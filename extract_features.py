ff8f2510092ead7be8aead02a083d9b220798acc
tazeek <tazeek.rakib@gmail.com>
2023-12-19 15:26:27 +0800

    Step by step code added

---
+++ extract_features.py
@@ -0,0 +1,22 @@
+
+
+# Load the dataset
+
+# Load the pre-trained models
+
+# Iterate by conversation
+
+# Iterate by utterance
+
+# Convert utterance to embeddings
+
+# Feed embeddings into the model
+
+# Get the features before the softmax
+
+# [Bonus] Convert features to VAD domain
+# Refer to the paper
+
+# Save features to dictionary
+
+# Save dictionary to either pickle or JSON
