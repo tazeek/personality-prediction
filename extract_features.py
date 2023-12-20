50d9daa21c4de33d0943dd765d3fffc5c3649511
tazeek <tazeek.rakib@gmail.com>
2023-12-20 12:36:19 +0800

    Extraction completed!

--- extract_features.py
+++ extract_features.py
@@ -1,12 +1,11 @@
 
 from transformers import BertTokenizer, BertModel
 from pathlib import Path
-
+from keras import backend as K
 from unseen_predictor import extract_bert_features
+
 import utils.dataset_processors as dataset_processors
-
 import tensorflow as tf
-
 import json
 import codecs
 import torch
@@ -14,10 +13,35 @@ import sys
 
 def get_personality_features(embeddings, personality_models):
      
+     personality_features = {}
+
      # Iterate trait by trait
-     # Get the features before the softmax
-     # Save in dictionary
-     ...
+     for trait, model in personality_models.items():
+         
+        #inp = model.input
+        #outputs = [layer.output for layer in model.layers]
+        #functors = [
+        #     K.function([inp, K.learning_phase()], [out])
+        #     for out in outputs
+
+        #]
+
+        #layer_outs = [
+        #    func([embeddings, 0.])
+        #    for func in functors
+        #]
+
+        outputs = [
+            K.function([model.input],[layer.output])([embeddings])
+            for layer in model.layers
+        ]
+
+        # Get the features before the softmax
+        personality_features[trait] = outputs[0][0][0]
+     
+     print(personality_features)
+     quit()
+     return personality_features
 
 def load_dataset():
      
@@ -38,6 +62,7 @@ def get_bert_model():
     return tokenizer, model
 
 def load_finetuned_models():
+
     directory = 'pkl_data/'
     fine_tuned_name = 'mlp_lm'
     trait_labels = ["EXT", "NEU", "AGR", "CON", "OPN"]
@@ -54,19 +79,20 @@ def load_finetuned_models():
 
     for trait in trait_labels:
             
-            model_name = f"{path_model}/MLP_LM_{trait}.h5"
-            print(f"Load model: {model_name}")
-            models[trait] = tf.keras.models.load_model(model_name)
+        model_name = f"{path_model}/MLP_LM_{trait}.h5"
+        models[trait] = tf.keras.models.load_model(model_name)
 
     return models
 
 def get_personalities_conversation(conversation, tokenizer, bert_model, personality_models):
      
+     
      for dialog in conversation:
           
         # Get the utterance
         utterance = dialog['utterance']
-
+        cleaned_utterance = dataset_processors.preprocess_text(utterance)
+        
         # Convert utterance to embeddings
         embeddings = extract_bert_features(
              utterance,
@@ -94,19 +120,32 @@ def get_personalities_conversation(conversation, tokenizer, bert_model, personal
 # Load the pre-trained models
 # - Big 5
 # - BERT
+print("Loading BERT models")
 bert_tokenizer, bert_model = get_bert_model()
+
+print("Loading OCEAN modes")
 models_ocean = load_finetuned_models()
 
 # Load the dataset
+print("Loading dataset")
 dataset = load_dataset()
 
 # Iterate by conversation
-for conversation in dataset:
+for index, conversation in enumerate(dataset):
      
     # Iterate by utterance
-     personalized_conversation_dict = get_personalities_conversation(conversation)
+    personalized_conversation_dict = get_personalities_conversation(
+          conversation,
+          bert_tokenizer,
+          bert_model,
+          models_ocean
+    )
+    break
 
-     # Append to list
+    # Append to list
+
+    if (index + 1) % 100 == 0:
+          print(f"{index + 1} conversations processed")
 
 # Save dictionary to either pickle or JSON
 # [Bonus] Save every utterance in a MongoDB record
