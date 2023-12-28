4a00ac984962f4c06aefc15a3f6365c196245fd2
tazeek <tazeek.rakib@gmail.com>
2023-12-28 10:14:54 +0800

    Add steps for fine tuning

---
+++ finetune_bert.py
@@ -0,0 +1,20 @@
+
+
+# Load dataset
+
+# Split dataset (6:2:2)
+
+# Load the BERT tokenizer
+
+# Tokenize features
+
+# Iterate per trait
+
+# Load the BERT model
+
+# Train per trait
+# Hyperparams:
+
+# Save the model
+
+
