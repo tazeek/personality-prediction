42384b6608495c3a210619931a16754e0b00cf13
tazeek <tazeek.rakib@gmail.com>
2023-12-28 10:38:20 +0800

    Load BERT model

--- finetune_bert.py
+++ finetune_bert.py
@@ -1,10 +1,12 @@
-
+from transformers import BertTokenizer, BertForSequenceClassification
 
 # Load dataset
 
 # Split dataset (6:2:2)
 
 # Load the BERT tokenizer
+model_name = "bert-base-uncased"
+tokenizer = BertTokenizer.from_pretrained(model_name)
 
 # Tokenize features
 
