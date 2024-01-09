fc7dfdd4909f02439b3e421b7bde4ebe8477e430
tazeek <tazeek.rakib@gmail.com>
2024-01-09 19:48:18 +0800

    Create new files

---
+++ sota_models.py
@@ -0,0 +1,13 @@
+
+
+# Load BERT tokenizer and model (fine-tuned)
+
+# Load the dataset and pre-process
+
+# Load the model required: LSTM, GRU, CNN
+
+# Train the model
+
+# Display the metrics
+
+# Save the model
