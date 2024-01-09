132218d3eec3197ac3be9bf4769132e7b7cbeac9
tazeek <tazeek.rakib@gmail.com>
2024-01-09 20:35:21 +0800

    Load the tokenizer and model

--- sota_models.py
+++ sota_models.py
@@ -1,11 +1,21 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
+import utils.dataset_processors as dataset_processors
 
 # Load BERT tokenizer and model (fine-tuned)
+model = AutoModelForSequenceClassification.from_pretrained('fine-tuned-bert-personality-sentence-segmentation')
+tokenizer = AutoTokenizer.from_pretrained('fine-tuned-bert-personality-sentence-segmentation')
 
 # Load the dataset and pre-process
 # Split using K-Fold cross validation (4)
+# Load the dataset
+datafile = "data/essays/essays.csv"
+dataset = dataset_processors.load_essays_df(datafile)
 
 # Prepare the training parameters
+learning_rate = 0.001
+batch_size = 32
+epochs = 20
 
 # Load the model required: LSTM, GRU, CNN
 
