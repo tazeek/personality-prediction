7029263cb6dea057504483d55f043e565141ad1e
tazeek <tazeek.rakib@gmail.com>
2024-01-13 18:30:55 +0800

    Making progress!

--- sota_list.py
+++ sota_list.py
@@ -6,27 +6,33 @@ class CNN(nn.Module):
     def __init__(self, output_size):
         super(CNN, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(1, 3))
-        self.conv2 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3))
-        self.conv3 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 1))
+        self.conv1 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1, 3), padding = (2, 0))
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3))
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 1))
 
-        self.fc1 = nn.Linear(32 * 3 * 3, 128)
+        self.fc1 = nn.Linear(764, 128)
         self.fc2 = nn.Linear(128, output_size)
 
     def forward(self, x):
 
-        x = x.unsqueeze(1)  # Add channel dimension for CNN
+        # Conv2D input: [batch_size, channels, height, width]
+        x = x.unsqueeze(1)
 
         x1 = torch.relu(self.conv1(x))
-        x2 = torch.relu(self.conv2(x))
-        x3 = torch.relu(self.conv3(x))
+        x2 = torch.relu(self.conv2(x1))
+        x3 = torch.relu(self.conv3(x2))
+
+        #x2 = torch.relu(self.conv2(x))
+        #x3 = torch.relu(self.conv3(x))
 
         # Concatenate outputs
-        x = torch.cat((x1, x2, x3), dim=1) 
+        #x = torch.cat((x1, x2, x3), dim=1) 
 
         # Flatten the tensor 
-        x = x.view(x.size(0), -1)  
+        #x = x.view(x.size(0), -1)  
         x = torch.relu(self.fc1(x))
+        print(x.size())
+        quit()
         x = self.fc2(x)
 
         return torch.sigmoid(x)
