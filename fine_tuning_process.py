b41df100528ad60d3d667f4c04242b8f199c5ed4
tazeek <tazeek.rakib@gmail.com>
2024-07-18 09:19:54 +0800

    Add the steps required

---
+++ fine_tuning_process.py
@@ -0,0 +1,15 @@
+
+
+# Load the LLM model to optimize
+
+# Load the dataset to optimize on
+
+# Define the type of approach (sentence or whole)
+
+# Split the dataset between train and test
+
+# Hyperparameters for LLM
+
+# Fine-tuning starts here
+
+
