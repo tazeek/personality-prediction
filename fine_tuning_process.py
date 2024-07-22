d5cb536c945412628db1046ecf4cc2ac4eb9b660
tazeek <tazeek.rakib@gmail.com>
2024-07-22 09:54:08 +0800

    Write code for loading

--- fine_tuning_process.py
+++ fine_tuning_process.py
@@ -7,15 +7,35 @@ def load_default_hyperparams(args):
 
 def load_llm_model(model_name):
 
-    # Load tokenizer and model
+    # Models used: BERT, RoBERTa, XLNet, ELECTRA, Albert
+    model_list = {
+        'bert': (BertModel, BertTokenizer, "bert-base-uncased"),
+        'roberta': (RobertaModel, RobertaTokenizer, "roberta-base"),
+        'xlnet': (XLNetModel, XLNetTokenizer, 'xlnet-base-cased'),
+        'electra': (ElectraModel, ElectraTokenizer, 'google/electra-base-discriminator'),
+        'albert': (AlbertModel, AlbertTokenizer, "albert-base-v2")
+    } 
 
-    ...
+    model_class, tokenizer_class, model_version = model_list[model_name]
+
+    tokenizer = tokenizer_class.from_pretrained(
+        model_version,
+        do_lower_case = True
+    )
+
+    model = model_class.from_pretrained(
+        model_version,
+        output_hidden_states = True
+    )
+
+    return model, tokenizer
 
 def load_dataset(dataset_name):
 
     # Load
 
     # Process
+    ...
 
 def splitting(dataset, ratio_split):
 
@@ -30,6 +50,7 @@ def fine_tuning_approach(approach_style, dataset):
     # Whole sentence
 
     # Sentence
+    ...
 
 def start_fine_tuning(model, epochs, train_set, test_set):
     ...
