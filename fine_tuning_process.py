16c22f22d2c4ee04ea62fe8e402941e852855c05
tazeek <tazeek.rakib@gmail.com>
2024-07-28 15:32:46 +0800

    Parser information added

--- fine_tuning_process.py
+++ fine_tuning_process.py
@@ -1,8 +1,22 @@
 from transformers import BertModel, BertTokenizer, RobertaModel, RobertaTokenizer, XLNetModel, XLNetTokenizer, ElectraModel, ElectraTokenizer, AlbertModel, AlbertTokenizer
 
-def load_default_hyperparams(args):
+import argparse
 
-    ...
+def load_default_hyperparams():
+
+    parser = argparse.ArgumentParser()
+
+    # Model related
+    parser.add_argument("--pretrained_model", "-pm", type=str, default="bert"
+        choices=["bert", "roberta", "xlnet", "electra", "albert"])
+    
+    # Hyperparameters for fine-tuning
+    parser.add_argument("--split", "--sp", type=float, default=0.7)
+    parser.add_argument("--dataset", "-ds", type=str, default="essays")
+    parser.add_argument("--epoch", "-ep", type=int, default=10)
+    parser.add_argument("--batch_size", "-bs", type=int, default=16)
+    
+    return parser.parse_args()
 
 
 def load_llm_model(model_name):
