9f466d09151881679a5ca7e9da6b8e9448c0dd72
tazeek <tazeek.rakib@gmail.com>
2024-10-18 20:39:48 +0800

    F1 and accuracy loaded

--- evaluator.py
+++ evaluator.py
@@ -1,9 +1,11 @@
-
+from sklearn.metrics import f1_score, accuracy_score, multilabel_confusion_matrix
 
 class Evaluator():
 
-    def __init__(self):
-        ...
+    def __init__(self, num_labels, threshold=0.5):
+
+        self._num_labels = num_labels
+        self._threshold = threshold
 
     def _save_file_pickle(self, data, file_name):
         ...
@@ -14,11 +16,20 @@ class Evaluator():
     def confusion_matrix(self, gold, predicted):
         ...
 
-    def calculate_accuracy(self, gold, predicted):
-        ...
+    def calculate_scores(self, gold, predicted):
 
-    def calculate_f1(self, gold, predicted):
-        ...
+        f1_scores = []
+        accuracy_scores = []
+
+        for i in range(self.num_labels):
+
+            f1_score = f1_score(gold[:, i], predicted[:, i])
+            accuracy_score = accuracy_score(gold[:, i], predicted[:, i])
+
+            accuracy_scores.append(accuracy_score)
+            f1_scores.append(f1_score)
+
+        return None
 
     def create_sample_predictions(self, input, predicted):
         ...
