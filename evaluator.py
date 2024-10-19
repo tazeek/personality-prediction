dcd6f0779995b78d2fe693c37946fd8cb3399565
tazeek <tazeek.rakib@gmail.com>
2024-10-19 12:11:42 +0800

    Convert the predictions

--- evaluator.py
+++ evaluator.py
@@ -1,4 +1,8 @@
 from sklearn.metrics import f1_score, accuracy_score, multilabel_confusion_matrix
+from torch.nn import Sigmoid
+
+import torch
+import numpy as np
 
 class Evaluator():
 
@@ -6,6 +10,17 @@ class Evaluator():
 
         self._num_labels = num_labels
         self._threshold = threshold
+
+        self._sigmoid = Sigmoid()
+
+    def _convert_predictions(self, pred_logits):
+        probs = self._sigmoid(torch.Tensor(pred_logits))
+
+        # Convert predictions to integer predictions
+        y_pred = np.zeros(probs.shape)
+        y_pred[np.where(probs >= self._threshold)] = 1
+
+        return y_pred
 
     def _save_file_pickle(self, data, file_name):
         ...
@@ -17,6 +32,8 @@ class Evaluator():
 
         f1_scores = []
         accuracy_scores = []
+
+        predicted = self._convert_predictions(predicted)
 
         for i in range(self.num_labels):
 
